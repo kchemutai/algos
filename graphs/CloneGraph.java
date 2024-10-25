94a8ab3d8c4936f93e09fb0b1f51f5f0ff5be426
Chemutai Kevin <kchemutai@gmail.com>
2024-10-24 22:57:40 -0600

    Clone Graph Iterative BFS

--- graphs/CloneGraph.java
+++ graphs/CloneGraph.java
@@ -2,17 +2,20 @@ package graphs;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class CloneGraph {
 
-    // recursive solution
-    public Node cloneGraph(Node node) {
-        if (node == null)
-            return null;
-        Map<Node, Node> oldToNew = new HashMap<>();
-        return dfs(node, oldToNew);
-    }
-
+    // recursive solution using DFS
+    /*
+     * public Node cloneGraph(Node node) {
+     * if (node == null)
+     * return null;
+     * Map<Node, Node> oldToNew = new HashMap<>();
+     * return dfs(node, oldToNew);
+     * }
+     */
     private Node dfs(Node root, Map<Node, Node> oldToNew) {
         if (oldToNew.containsKey(root))
             return oldToNew.get(root);
@@ -23,4 +26,33 @@ public class CloneGraph {
         }
         return copy;
     }
+
+    public Node cloneGraph(Node node) {
+        if (node == null)
+            return null;
+        Map<Node, Node> oldToNew = new HashMap<>();
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(node);
+        oldToNew.put(node, new Node(node.val));
+        while (!queue.isEmpty()) {
+            Node current = queue.poll();
+            Node copy = oldToNew.get(current);
+            for (Node neighbor : current.neighbors) {
+                Node newNode;
+                if (oldToNew.containsKey(neighbor)){
+                   newNode = oldToNew.get(neighbor);
+                }
+                else{
+                    queue.add(neighbor);
+                    newNode = new Node(neighbor.val);
+                    oldToNew.put(neighbor, newNode);
+                }
+                copy.neighbors.add(newNode);
+            }
+
+        }
+        return oldToNew.get(node);
+    }
+
 }
